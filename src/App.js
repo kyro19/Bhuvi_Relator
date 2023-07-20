9c6e1c7d86ece9530b8e0c06fad066f85144d29b
kyro19 <bhuvu.kyro19@gmail.com>
2023-07-20 16:37:50 +0530

    Created header component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import ForgotPassword from "./Pages/ForgotPassword";
 import Signin from "./Pages/Signin";
 import Signup from "./Pages/Signup";
 import Offers from "./Pages/Offers";
+import Header from "./Components/Header";
 function App() {
   return (
     <>
 
       <Router>
+        <Header/>
         <Routes>
            <Route path="/" element = {<Home/>}/>
            <Route path="/profile" element = {<Profile/>}/>
